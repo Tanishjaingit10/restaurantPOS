743080f640de75e9d02b57c346c6dd562bb367d4
SakshiVijay24 <70104243+SakshiVijay24@users.noreply.github.com>
2021-10-19 20:30:19 +0530

    Update ItemForm.js

--- client/src/components/ItemForm.js
+++ client/src/components/ItemForm.js
@@ -13,33 +13,61 @@ const ItemForm = () => {
     const [showDays, setShowDays] = useState(false);
     const [showTime, setShowTime] = useState(false);
     const [variant, setVariant] = useState(false);
-   
-    // const [clock, setClock] = useState("");
-    const openDrop = () => {
-        setShow(!show);
-    }
-
-    let clock,st,et;
+    const [showAvailable, setShowAvailable] = useState(false);
+    const [availabilty, setAvailability] = useState({ day: "", startTime: "", endTime: "" });
+    const [list, setList] = useState();
+ 
+    let name,value;
+    let st,et;
     const showStart = (e) => {
         st = e;
+        name = "startTime";
+        value = e;
+        console.log(st);
+        setAvailability({ ...availabilty, [name]: value });
     }
 
     const showEnd = (e)=>{
         et = e;
+        name = "endTime";
+        value = e;
+        setAvailability({ ...availabilty, [name]: value });
+        console.log(et);
+    }
+ 
+
+    const handleDay = (e) => {
+        e.preventDefault();
+        setShowDays(!showDays);
+        setShowTime(!showTime)
+        name = e.target.name;
+        value = e.target.value;
+        setAvailability({ ...availabilty, [name]: value });
     }
 
     const showWeek = () => {
         setShowDays(!showDays);
         setDisplayDays(
             days.map((obj) => {
-                return <button value={obj.day} className="bg-primary p-2 text-white text-left px-6" onClick={() => setShowTime(!showTime)}>{obj.day}</button>
+                return <button value={obj.day} name="day" className="bg-primary p-2 text-white text-left px-6" onClick={handleDay}>{obj.day}</button>
             })
         )
 
     }
+     
+    const showDayTime = ()=> {
+        setShowDays(!showDays);
+        setShowAvailable(!showAvailable);
+        setList(
+           <button className="bg-primary px-10 py-2">{availabilty.day} | {availabilty.startTime} - {availabilty.endTime}</button>
+        )
+       
+    }
 
-    const openPop = () => {
-        setIsOpen(!isOpen);
+    const dayTime = (e)=> {
+        setShowTime(!showTime)
+        
+        console.log(availabilty);
     }
 
     return (
@@ -69,9 +97,9 @@ const ItemForm = () => {
                             </div>
                             <div className="flex flex-col bg-white">
                                 <label htmlFor="description" className="mb-2">Available</label>
-                                <ul className="bg-primary text-center text-white" onClick={openDrop}><li className="py-2">Select</li>
+                                <ul className="bg-primary text-center text-white" onClick={()=>{setShow(!show)}}><li className="py-2">Select</li>
                                     {show ? <><li className="py-2">Everyday/All Time</li>
-                                        <li className="py-2" onClick={openPop}>Select Day/Time</li></> : null}
+                                        <li className="py-2" onClick={()=>{setIsOpen(!isOpen)}}>Select Day/Time</li></> : null}
                                 </ul>
                             </div>
                             <div className="flex flex-row bg-white space-x-4">
@@ -114,16 +142,17 @@ const ItemForm = () => {
             {isOpen && <Popup
                 content={<>
                     <div className="flex flex-col px-8 space-y-4 text-white">
+                        {showAvailable ? <div className="">{list}</div>:null}
                         <button className="bg-green px-10 py-2" onClick={showWeek}>+</button>
-                        <button className="bg-green px-10 py-2" onClick={openPop}>Done</button>
+                        <button className="bg-green px-10 py-2" onClick={()=>{setIsOpen(!isOpen)}}>Done</button>
                     </div>
                 </>}
-                handleClose={openPop}
+                handleClose={()=>{setIsOpen(!isOpen)}}
             />}
             {showDays && <div className="popup-box">
                 <div className="flex flex-col w-80 mx-auto font-roboto font-bold mt-52">
                     {displayDays}
-                    <button className="bg-green p-2 text-white text-center font-bold px-6" onClick={showWeek}>Done</button>
+                    <button className="bg-green p-2 text-white text-center font-bold px-6" onClick={showDayTime}>Done</button>
                 </div>
             </div>}
             {showTime && <div className="popup-box">
@@ -131,29 +160,28 @@ const ItemForm = () => {
                     <div className="flex flex-row py-2">
                     <label className="text-white w-1/2 ml-2">Start Time:</label>
                     <TimePicker
-                        time={st}
+                        placeholder={availabilty.startTime}
                         theme="Bourbon"
                         className="timepicker bg-primary text-white"
-                  
                         onSet={(val) => {
                             showStart(val.format12);
                           }}
+                       
                     />
                     </div>
                     <div className="flex flex-row py-2">
                     <label className="text-white w-1/2 ml-2">End Time:</label>
                       <TimePicker
-                        time={et}
+                       placeholder={availabilty.endTime}
                         theme="Bourbon"
                         className="timepicker bg-primary text-white"
-                       
                         onSet={(val) => {
                             showEnd(val.format12);
                           }}
                     />
                     </div> 
 
-                    <button className="bg-green p-2 text-white text-center font-bold px-6" onClick={() => setShowTime(!showTime)}>Done</button>
+                    <button className="bg-green p-2 text-white text-center font-bold px-6" onClick={dayTime}>Done</button>
                 </div>
             </div>}
             
