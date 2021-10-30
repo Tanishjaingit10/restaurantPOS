d802bd0c0d62966316d4dbe99a2d3bad79e7a317
hitanshijain27 <hitanshijain27@gmail.com>
2021-10-30 13:24:19 +0530

    Update CategoryList.js

--- client/src/components/PosContainer.js/CategoryList.js
+++ client/src/components/PosContainer.js/CategoryList.js
@@ -5,11 +5,14 @@ import { setCategories } from "../../actions/CategoryActions";
 const CategoryList = () => {
   const categories = useSelector((state) => state.allCategories.categories);
   const [displayItems, setDisplayItems] = useState();
-  const [order, showOrder] = useState({image: "", finalVariant: [],price: 0});
-    const [Variant, setVariant] = useState();
-    const [finalVar, setFinalVariant]=useState([]);
-    const [addList, setAddList] = useState();
-    const [list, showList] = useState(false);
+  const [order, showOrder] = useState({ foodItem: "", image: "", finalVariant: [], variants: [], price: 0,subtotal:0 });
+  // const [finalOrder, showfinalOrder] = useState({ foodItem: "", price: 0, , subtotal: "" })
+  const [Variant, setVariant] = useState([]);
+  const [finalVar, setFinalVariant] = useState([]);
+  const [addList, setAddList] = useState();
+  const [final, setFinal] = useState();
+  const [list, showList] = useState(false);
+  const [show, setShow] = useState(false)
   const showItems = async (e) => {
 
     const result = await fetch('/app/items').then((res) => res.json())
@@ -17,7 +20,7 @@ const CategoryList = () => {
         console.log(json)
         setDisplayItems(json.map((option) => {
           if (option.category === e.target.value) {
-            return (<div className="bg-white m-2 relative rounded-xl shadow-2xl w-40 cursor-pointer" onClick={()=>setOrder(option.foodItem)}><img src={option.image} className="w-40 object-fill" alt="" />
+            return (<div className="bg-white m-2 relative rounded-xl shadow-2xl w-40 cursor-pointer" onClick={() => setOrder(option.foodItem)}><img src={option.image} className="w-40 object-fill" alt="" />
               <span className="absolute right-0 top-0 text-center w-20 py-2 bg-white">{option.price}</span>
               <div className="text-center bg-white py-2">{option.foodItem}</div></div>)
           }
@@ -26,52 +29,49 @@ const CategoryList = () => {
       })
 
   }
-  const showVariant = async(e)=>{
-    console.log(e)
+  const handleVariant = async (e) => {
+    setVariant(e);
+    showList(false)
+    order.subtotal = order.subtotal + e.price;
     setFinalVariant(oldArray => [...oldArray, Variant])
-    showList(!list);
+  }
+  const showVariant = async (e) => {
+    // console.log(finalVar)
+    // console.log(Variant.variant)
+    // console.log(e)
+    showList(true);
     setAddList(
-        e.map((obj)=>{
-        return (<button className="bg-primary px-10 py-2 w-full relative"><a>{obj.variant} / $ {obj.price}</a><span className="absolute right-4">x</span></button>)
-        }))
-}
+      e.map((obj) => {
+        console.log(obj)
+        return (<li><button className="bg-primary px-10 py-2 w-full relative" onClick={() => handleVariant(obj)} name="category" value={obj}><a>{obj.variant} / $ {obj.price}</a><span className="absolute right-4">x</span></button></li>)
+      }))
+  }
 
+  const addCart = async (e) => {
 
+  }
   const setOrder = async (e) => {
-    if(e){
-    const result = await fetch(`/app/item/${e}`).then((res) => res.json())
-      .then((json) => {
-        console.log(json)
-        console.log(json.finalVariant)
-        showOrder({
-          "image": json.image,
-          "finalVariant": json.finalVariant,
-          "price": json.price
+    if (e) {
+      const result = await fetch(`/app/item/${e}`).then((res) => res.json())
+        .then((json) => {
+          // console.log(json)
+          // console.log(json.finalVariant)
+          showOrder({
+            "foodItem": json.foodItem,
+            "image": json.image,
+            "finalVariant": json.finalVariant,
+            "variants": [],
+            "price": json.price,
+            "subtotal":json.price
+          })
         }
-          // <div className="absolute top-16 right-0 bg-white border-l-2 border-primary w-2/5 h-full">
-          //   <div className="w-72 mt-6 bg-gray-500 mx-auto h-36"><img src={json.image} /></div>
-          //   <h1 className="text-gray-500 text-left text-xl my-4 font-semibold font-roboto ml-4">Select Quantity</h1>
-           
-          //   <div className="flex flex-col px-8 space-y-4 text-xl font-roboto py-4">
-          //     {list ? <div>{addList}</div>:null}
-          //     <button className="bg-green text-center text-white py-2 font-bold" onClick={() => showVariant(json.finalVariant)}>+</button>
-          //   </div>
-          //   <div className="bg-gray-300 w-full relative text-gray-600 px-6 py-8 text-xl font-bold font-roboto">
-          //     <label>Subtotal</label><span className="absolute right-4">{json.price}</span>
-          //   </div>
-          //   <div className="px-8 py-4">
-          //     <button className="bg-green text-white text-center text-lg font-bold w-full py-2">Add to Cart</button>
-          //   </div>
-          // </div>
-        )
-      }
 
-      )
+        )
     }
 
   }
 
-  
+
 
   const renderList = categories.map((cat) => {
     const { category, description, color } = cat;
@@ -93,14 +93,19 @@ const CategoryList = () => {
   };
   useEffect(() => {
     fetchCategories();
-    
   }, []);
+  useEffect(() => {
+    setFinal(finalVar.map((obj) => {
+      return (<button className="bg-primary px-10 py-2 w-full mb-2 relative"><a>{obj.variant} / $ {obj.price}</a><span className="absolute right-4">x</span></button>)
+    }))
+  }, [finalVar]);
 
- 
+
+
   console.log("Categories:", categories);
 
   return (
-  
+
     <div>
 
       <div className="flex flex-wrap justify-evenly">
@@ -112,23 +117,27 @@ const CategoryList = () => {
       {/* <div>
         {order}
       </div> */}
-    
-      {order.image?<div className="absolute top-16 right-0 bg-white border-l-2 border-primary w-2/5 h-full">
-            <div className="w-72 mt-6 bg-gray-500 mx-auto h-36"><img src={order.image} /></div>
-            <h1 className="text-gray-500 text-left text-xl my-4 font-semibold font-roboto ml-4">Select Quantity</h1>
-           
-            <div className="flex flex-col px-8 space-y-4 text-xl font-roboto py-4">
-           
-              {list ? <div>{addList}</div>:null}
-              <button className="bg-green text-center text-white py-2 font-bold" onClick={() => showVariant(order.finalVariant)}>+</button>
-            </div>
-            <div className="bg-gray-300 w-full relative text-gray-600 px-6 py-8 text-xl font-bold font-roboto">
-              <label>Subtotal</label><span className="absolute right-4">{order.price}</span>
-            </div>
-            <div className="px-8 py-4">
-              <button className="bg-green text-white text-center text-lg font-bold w-full py-2">Add to Cart</button>
-            </div>
-          </div>:null}
+
+      {order.image ? <div className="absolute top-16 right-0 bg-white border-l-2 border-primary w-2/5 h-full">
+        <div className="w-72 mt-6 bg-gray-500 mx-auto h-36"><img src={order.image} /></div>
+        <h1 className="text-gray-500 text-left text-xl my-4 font-semibold font-roboto ml-4">Select Quantity and Variant</h1>
+
+        <div className="flex flex-col px-8 space-y-4 text-xl font-roboto py-4">
+          {final ? <div className="text-white w-full">{final}</div> : null}
+
+
+          {list ? <ul className="bg-primary text-center py-2 text-white cursor-pointer" onClick={() => { setShow(!show) }}>{'Select Variant'}
+            {show ? <>{addList}</> : null}
+          </ul> : null}
+          <button className="bg-green text-center text-white py-2 font-bold" onClick={() => showVariant(order.finalVariant)}>+</button>
+        </div>
+        <div className="bg-gray-300 w-full relative text-gray-600 px-6 py-8 text-xl font-bold font-roboto">
+          <label>Subtotal</label><span className="absolute right-4">{order.subtotal}</span>
+        </div>
+        <div className="px-8 py-4">
+          <button className="bg-green text-white text-center text-lg font-bold w-full py-2" onClick={addCart}>Add to Cart</button>
+        </div>
+      </div> : null}
 
     </div>
   );
