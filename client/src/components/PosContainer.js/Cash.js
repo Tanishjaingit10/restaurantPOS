799c020266fa979764444a4c90756835f09b0474
hitanshijain27 <hitanshijain27@gmail.com>
2021-10-31 20:12:20 +0530

    calculator

--- client/src/components/PosContainer.js/Cash.js
+++ client/src/components/PosContainer.js/Cash.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Cash = () => {
+  const [cal,setCal]=useState(0)
+  const calculate = async (e) => {
+    if(e===11)
+      setCal(100*cal)
+    else if(e===12)
+      setCal(Math.floor(cal/10))
+    else
+    setCal(10*cal+e)
+  }
   return (
     <div className="h-screen justify-items-conter">
       <nav className="bg-gray-400 py-6 px-1 mt-0 h-auto w-full top-0 text-2xl">
@@ -21,21 +30,21 @@ const Cash = () => {
           <div className="text-center mt-2 text-3xl text-primary">$14.00</div>
         </div>
         <div className="w-1/3 mx-auto flex flex-col shadow-2xl">
-            <div className="bg-white relative py-4 px-2 font-roboto"><p>Amount tendered</p><span className="absolute top-2 right-2 text-4xl font-bold">$0.00<i className="far fa-times-circle ml-2 text-2xl"></i></span></div>
-            <div className="bg-gray-300 justify-evenly flex flex-row text-3xl py-2 text-primary font-semibold"><div className="text-center">Exact</div><div className="text-center">$10.00</div><div className="text-center">$10.00</div></div>
+            <div className="bg-white relative py-4 px-2 font-roboto"><p>Amount tendered</p><span className="absolute top-2 right-2 text-4xl font-bold">${cal}<i onClick = {() => { setCal(0) }} className="far fa-times-circle ml-2 text-2xl cursor-pointer"></i></span></div>
+            <div className="bg-gray-300 justify-evenly flex flex-row text-3xl py-2 text-primary font-semibold"><div className="text-center">Exact</div><button onClick = {() => { setCal(10) }} className="text-center">$10.00</button><button onClick = {() => { setCal(20) }} className="text-center">$20.00</button></div>
             <div className="grid grid-cols-3 bg-white font-roboto font-semibold text-2xl">
-                <div className="text-center border-2 py-2">1</div>
-                <div className="text-center border-2 py-2">2</div>
-                <div className="text-center border-2 py-2">3</div>
-                <div className="text-center border-2 py-2">4</div>
-                <div className="text-center border-2 py-2">5</div>
-                <div className="text-center border-2 py-2">6</div>
-                <div className="text-center border-2 py-2">7</div>
-                <div className="text-center border-2 py-2">8</div>
-                <div className="text-center border-2 py-2">9</div>
-                <div className="text-center border-2 py-2">0</div>
-                <div className="text-center border-2 py-2">00</div>
-                <div className="text-center border-2 py-2"><i class="fas fa-backspace"></i></div>
+                <button onClick = {() => { calculate(1) }} className="text-center border-2 py-2">1</button>
+                <button onClick = {() => { calculate(2) }} className="text-center border-2 py-2">2</button>
+                <button onClick = {() => { calculate(3) }} className="text-center border-2 py-2">3</button>
+                <button onClick = {() => { calculate(4) }} className="text-center border-2 py-2">4</button>
+                <button onClick = {() => { calculate(5) }} className="text-center border-2 py-2">5</button>
+                <button onClick = {() => { calculate(6) }} className="text-center border-2 py-2">6</button>
+                <button onClick = {() => { calculate(7) }} className="text-center border-2 py-2">7</button>
+                <button onClick = {() => { calculate(8) }} className="text-center border-2 py-2">8</button>
+                <button onClick = {() => { calculate(9) }} className="text-center border-2 py-2">9</button>
+                <button onClick = {() => { calculate(0) }} className="text-center border-2 py-2">0</button>
+                <button onClick = {() => { calculate(11) }} className="text-center border-2 py-2">00</button>
+                <div className="text-center border-2 py-2"><i onClick= {() => { calculate(12) }} className="fas fa-backspace cursor-pointer"></i></div>
             </div>
         </div>
         <div className="w-96 bg-green mt-10 mx-auto py-4 text-white text-center text-2xl font-roboto font-bold">Proceed</div>
--- client/src/components/PosContainer.js/Discount.js
+++ client/src/components/PosContainer.js/Discount.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Discount = () => {
+  const [cal,setCal]=useState(0)
+  const calculate = async (e) => {
+    if(e===11)
+      setCal(100*cal)
+    else if(e===12)
+      setCal(Math.floor(cal/10))
+    else
+    setCal(10*cal+e)
+  }
     return (
         <div className="h-screen justify-items-conter">
       <nav className="bg-gray-400 py-6 px-1 mt-0 h-auto w-full top-0 text-2xl">
@@ -18,21 +27,21 @@ const Discount = () => {
       <div className="flex flex-col p-8">
       
         <div className="w-1/3 mx-auto flex flex-col shadow-2xl mt-10">
-            <div className="bg-white px-2 font-roboto py-4"><div className=" text-right text-4xl font-bold">0%<i className="far fa-times-circle ml-4 text-2xl"></i></div></div>
-            <div className="bg-gray-300 justify-evenly flex flex-row text-2xl py-4 text-primary font-bold border-2"><div className="text-center w-full">10%</div><div className="text-center w-full">15%</div><div className="text-center w-full">20%</div></div>
+            <div className="bg-white px-2 font-roboto py-4"><div className=" text-right text-4xl font-bold">{cal}%<i onClick = {() => { setCal(0) }} className="far fa-times-circle ml-4 text-2xl cursor-pointer"></i></div></div>
+            <div className="bg-gray-300 justify-evenly flex flex-row text-2xl py-4 text-primary font-bold border-2"><button className="text-center w-full" onClick = {() => { setCal(10) }} >10%</button><button className="text-center w-full" onClick = {() => { setCal(15) }}>15%</button><button className="text-center w-full" onClick = {() => { setCal(20) }}>20%</button></div>
             <div className="grid grid-cols-3 bg-white font-roboto font-semibold text-2xl">
-                <div className="text-center border-2 py-2">1</div>
-                <div className="text-center border-2 py-2">2</div>
-                <div className="text-center border-2 py-2">3</div>
-                <div className="text-center border-2 py-2">4</div>
-                <div className="text-center border-2 py-2">5</div>
-                <div className="text-center border-2 py-2">6</div>
-                <div className="text-center border-2 py-2">7</div>
-                <div className="text-center border-2 py-2">8</div>
-                <div className="text-center border-2 py-2">9</div>
-                <div className="text-center border-2 py-2">0</div>
-                <div className="text-center border-2 py-2">00</div>
-                <div className="text-center border-2 py-2"><i class="fas fa-backspace"></i></div>
+            <button onClick = {() => { calculate(1) }} className="text-center border-2 py-2">1</button>
+                <button onClick = {() => { calculate(2) }} className="text-center border-2 py-2">2</button>
+                <button onClick = {() => { calculate(3) }} className="text-center border-2 py-2">3</button>
+                <button onClick = {() => { calculate(4) }} className="text-center border-2 py-2">4</button>
+                <button onClick = {() => { calculate(5) }} className="text-center border-2 py-2">5</button>
+                <button onClick = {() => { calculate(6) }} className="text-center border-2 py-2">6</button>
+                <button onClick = {() => { calculate(7) }} className="text-center border-2 py-2">7</button>
+                <button onClick = {() => { calculate(8) }} className="text-center border-2 py-2">8</button>
+                <button onClick = {() => { calculate(9) }} className="text-center border-2 py-2">9</button>
+                <button onClick = {() => { calculate(0) }} className="text-center border-2 py-2">0</button>
+                <button onClick = {() => { calculate(11) }} className="text-center border-2 py-2">00</button>
+                <div className="text-center border-2 py-2"><i onClick= {() => { calculate(12) }} className="fas fa-backspace cursor-pointer"></i></div>
             </div>
         </div>
         <div className="w-96 bg-green mt-10 mx-auto py-4 text-white text-center text-2xl font-roboto font-bold">Proceed</div>
