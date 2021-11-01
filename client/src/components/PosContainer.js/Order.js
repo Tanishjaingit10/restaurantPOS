b4e2c1909f514ef2ee98ea01d2b63ae76543f22e
SakshiVijay24 <70104243+SakshiVijay24@users.noreply.github.com>
2021-11-01 16:49:18 +0530

    Update Order.js

--- client/src/components/PosContainer.js/Order.js
+++ client/src/components/PosContainer.js/Order.js
@@ -1,23 +1,43 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState, useEffect} from 'react'
 import { OrderContext } from '../../context/Auth'
 
 const Order = () => {
-    const [cart, setCart] = useContext(OrderContext)
-  
-    return (
-        <div className="flex flex-row p-4 border-2 font-roboto">
+    const [cart, setCart] = useContext(OrderContext);
+    const [display, setDisplay] = useState();
+    useEffect(()=>{
+     setDisplay(
+       cart.map((order)=>{
+         return (
+          <div className="flex flex-row p-4 border-2 font-roboto">
           <div className=" w-1/3 text-left flex flex-col pl-6">
-           <div className="text-lg pb-2">Food Item Name</div>
-           <div className="text-gray-400 font-semibold text-md">Variant</div>
+           <div className="text-lg pb-2">{order.foodItem}</div>
+           {order.orderedVariant.map((obj)=>{
+             return (
+              <div className="text-gray-400 font-semibold text-md">{obj.variant}</div>
+             )
+           })}
           </div>
           <div className=" w-1/3 text-center flex flex-col">
-           <div className="text-lg pb-2">Price</div>
-           <div className="text-gray-400 font-semibold text-md">Variant Price</div>
+           <div className="text-lg pb-2">{order.price}</div>
+           {order.orderedVariant.map((obj)=>{
+             return (
+              <div className="text-gray-400 font-semibold text-md">{obj.price}</div>
+             )
+           })}
+          
           </div>
           <div className=" w-1/3 text-center my-auto text-lg">
-           Subtotal<span><i className="far fa-times-circle ml-8"></i></span>
+           {order.subtotal}<span><i className="far fa-times-circle ml-8"></i></span>
           </div>
         </div>
+         )
+       })
+     )
+    },[cart])
+    return (
+      <>
+       {display}
+       </>
     )
 }
 
