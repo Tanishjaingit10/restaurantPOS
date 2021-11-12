ca878f489e506401b20609c116bb9e4068294706
hitanshijain27 <hitanshijain27@gmail.com>
2021-11-12 14:22:41 +0530

    -

--- client/src/components/Content.js
+++ client/src/components/Content.js
@@ -4,10 +4,10 @@ const Content = () => {
     return (
         <div className="mt-24 overflow-x-hidden flex flex-col w-full px-10">
             <div className="w-full flex flex-wrap mx-auto pl-20 text-white text-3xl text-center font-semibold">
-                <div className="bg-green w-64 h-32 m-10 p-2">
+                <a href='/orders' className="bg-green w-64 h-32 m-10 p-2">
                     <p>Lifetime Orders</p>
                     <p className="text-5xl">127</p>
-                </div>
+                </a>
                 <div className="bg-brown w-64 h-32 m-10 p-2">
                     <p>Today Orders</p>
                     <p className="text-5xl">3</p>
@@ -16,10 +16,10 @@ const Content = () => {
                     <p>Today Sale</p>
                     <p className="text-5xl">$127</p>
                 </div>
-                <div className="bg-blue w-64 h-32 m-10 p-2">
+                <a href='/customers' className="bg-blue w-64 h-32 m-10 p-2">
                     <p>Total Customers</p>
                     <p className="text-5xl">5.6k</p>
-                </div>
+                </a>
                 <div className="bg-yellow w-64 h-32 m-10 p-2">
                     <p>Total Takeaways</p>
                     <p className="text-5xl">65</p>
--- client/src/components/Counter.js
+++ client/src/components/Counter.js
@@ -2,15 +2,14 @@ import React, { useState, useEffect } from "react";
 let arr = new Array(1000000).fill(false);
 const Counter = () => {
   const [display, setDisplay] = useState();
-  const [Order, showOrder] = useState();
   const showDetails = (e, index) => {
     arr[index] = !arr[index];
     if (arr[index]) {
       arr = arr.map(x => false);
-      arr[index]=true;
+      arr[index] = true;
     }
   };
- 
+
   var code;
   const loadOrders = async (e) => {
     await fetch("/app/orders")
@@ -18,10 +17,10 @@ const Counter = () => {
       .then((json) => {
         setDisplay(
           json.map((option, index) => {
-            if(index%2===0)
-              code="#1DBE19";
+            if (option.payment[0].orderStatus === 'Ready to Serve')
+              code = "#1DBE19";
             else
-                code="#BE2D19"
+              code = "#BE2D19"
             return (
               <div
                 className={
@@ -31,7 +30,7 @@ const Counter = () => {
                 }
               >
                 <div className="flex flex-col font-bold">
-                  <div className="flex flex-row text-xl text-white font-roboto" style={{backgroundColor : code}}>
+                  <div className="flex flex-row text-xl text-white font-roboto" style={{ backgroundColor: code }}>
                     <div className=" w-full p-4 text-left">
                       Table No. {option.payment[0].table}
                     </div>
@@ -39,7 +38,7 @@ const Counter = () => {
                       {option.payment[0].orderType}
                     </div>
                   </div>
-                  <div className="flex flex-row text-xl text-white font-roboto " style={{backgroundColor : code}}>
+                  <div className="flex flex-row text-xl text-white font-roboto " style={{ backgroundColor: code }}>
                     <div className=" w-full p-4 text-left">Token No. 302</div>
                     <div className=" w-full p-4 text-right">
                       #{option.order_id}
@@ -47,11 +46,11 @@ const Counter = () => {
                   </div>
                   <div
                     className="relative rounded-full bottom-6 p-2 bg-white w-12 h-12 mx-auto shadow-lg text-center text-xl"
-                    style={{color : code}}
+                    style={{ color: code }}
                     onClick={() => {
                       showDetails(option, index);
                     }}
-                    
+
                   >
                     {arr[index] ? (
                       <i className="fas fa-chevron-up mt-2"></i>
@@ -67,26 +66,26 @@ const Counter = () => {
                         {
                           option.order.map((obj) => {
                             return (
-                            
-                                <div className="flex flex-col w-full py-2">
-                                  <div className="text-xl font-semibold">{obj.foodItem}</div>
-                                  {obj.orderedVariant.map((extra) => {
-                                    return (
-                                      <>
-                                        <div className="text-md text-gray-400 font-medium">
-                                          1 x {extra.variant}
-                                        </div>
-                                      </>
-                                    );
-                                  })}
-                                </div>)
-                        })}
+
+                              <div className="flex flex-col w-full py-2">
+                                <div className="text-xl font-semibold">{obj.foodItem}</div>
+                                {obj.orderedVariant.map((extra) => {
+                                  return (
+                                    <>
+                                      <div className="text-md text-gray-400 font-medium">
+                                        1 x {extra.variant}
+                                      </div>
+                                    </>
+                                  );
+                                })}
+                              </div>)
+                          })}
                       </div>
-                      <div className="flex flex-col text-xl text-right font-semibold w-full" style={{color : code}}>
+                      <div className="flex flex-col text-xl text-right font-semibold w-full" style={{ color: code }}>
                         <div className="py-2">
                           {option.payment[0].orderStatus}
                         </div>
-                        <div className="py-2">00:05:00</div>
+                        <div className="py-2">{option.payment[0].timeToCook? option.payment[0].timeToCook:'0:00'}</div>
                       </div>
                     </div>
                   </>
