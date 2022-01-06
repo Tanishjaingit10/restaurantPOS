54a6defdb86f4e806020ef5ca420d48efd49a90e
tanish garg <tanishg566@outlook.com>
2022-01-06 21:16:50 +0530

    Login and Register Screen UI Part Done

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,108 +1,155 @@
-import React, { useState } from 'react'
-import { useHistory } from 'react-router-dom';
-import options from '../levels';
-import Logo from '../images/logo.jpeg';
-import signin from '../signin';
-import Popup from './Popup';
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import options from "../levels";
+import Logo from "../images/logo.jpeg";
+import signin from "../signin";
+import Popup from "./Popup";
+import chefLogin from "../images/chefLogin.png";
 
 const Login = () => {
-    const history = useHistory();
-    const [email_id, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [position, setPosition] = useState('');
-    const [isOpen, setIsOpen] = useState(false);
-    const [msg, setMsg] = useState("");
+  const history = useHistory();
+  const [email_id, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [position, setPosition] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+  const [msg, setMsg] = useState("");
 
-    const loginUser = async (e) => {
-        e.preventDefault();
+  const loginUser = async (e) => {
+    e.preventDefault();
 
-        const res = await fetch("/app/signin", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                position,email_id, password
-            })
-        });
+    const res = await fetch("/app/signin", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        position,
+        email_id,
+        password,
+      }),
+    });
 
-        setIsOpen(!isOpen);
-        if(res.status===201)
-        {
-            history.push("/home");
-        }
-        else
-        {
-            let obj = signin.find((pop) => pop.id === res.status);
-            setMsg(obj.title);
-        }
+    setIsOpen(!isOpen);
+    if (res.status === 201) {
+      history.push("/home");
+    } else {
+      let obj = signin.find((pop) => pop.id === res.status);
+      setMsg(obj.title);
     }
+  };
 
-    return (
-        <div className="h-screen">
-        <div className="bg-gray-300 py-2">
-        <img className="mx-auto w-36 h-auto" src={Logo} alt=""/>
-        </div>
-        <div>
-            <form className="w-1/2 md:w-1/3 mx-auto py-10 font-bold font-roboto text-lg">
-                <div className="shadow-2xl">
-                <div className="bg-white px-6 py-4">
-                <label className="text-left text-black mr-4">Enter as</label>
-                <select name="position" className ="p-2 border-black border-2 w-4/5 text-md" onChange={(e) => setPosition(e.target.value)} value={position}>
-                            {options.map((option)=>(
-                                <option value={option.value} className="bg-gray-300 border-black border-2 p-2">{option.label}</option>
+  return (
+    <div className="h-screen">
+      <div className="bg-white py-2 ">
+        <img className="mx-auto w-28 h-auto mb-4 " src={Logo} alt="" />
+      </div>
+      <div className="flex flex-row h-auto m-auto bg-white w-3/4 rounded-3xl shadow-2xl">
+        <img src={chefLogin} />
+        <form className="w-1/2 md:w-1/3 mx-auto py-10 font-bold font-roboto text-lg">
+          <div>
+            <div className="bg-white px-6 py-4 ">
+              <div className="w-full justify-center">
+                <h1 className="text-red text-center text-2xl">Login</h1>
+              </div>
+              <label className="text-left text-black mr-4 font-normal">
+                Enter as
+              </label>
+              <select
+                name="position"
+                className="p-2  border-2 w-full text-md bg-red rounded-lg text-white font-thin"
+                onChange={(e) => setPosition(e.target.value)}
+                value={position}
+              >
+                <option value="none" selected="selected" hidden>
+                  Select Operator Type
+                </option>
+                {options.map((option) => (
+                  <option
+                    value={option.value}
+                    className="bg-gray-300 border-black border-2 p-2"
+                  >
+                    {option.label}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="bg-white px-6 pt-4">
+              <label className="text-left font-normal">Enter User Id</label>
+              <input
+                name="email_id"
+                placeholder="Enter User ID"
+                type="email"
+                value={email_id}
+                onChange={(e) => setEmail(e.target.value)}
+                className={
+                  "w-full p-2 rounded-lg border-gray-200 border-2 outline-none text-sm transition duration-150 ease-in-out mb-4 mt-2"
+                }
+              />
+            </div>
+            <div className="bg-white px-6 pb-4">
+              <label className=" text-left font-normal">Password</label>
+              <input
+                name="password"
+                placeholder="Password / Employee Code"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className={
+                  "w-full p-2 rounded-lg border-gray-200 border-2 outline-none text-sm transition duration-150 ease-in-out mb-4 mt-2"
+                }
+              />
+            </div>
+            <div className="flex flex-row justify-between">
+              <div className="mx-6">
+                <input
+                  type="checkbox"
+                  id="remember"
+                  name="remember"
+                  value="Remember Me"
+                />
+                <span className="text-red text-base font-thin">
+                  {"  "}Remember Me
+                </span>
+              </div>
+              <a className="text-red text-base font-thin">
+                Forgot ID / Password?
+              </a>
+            </div>
+          </div>
+          <div className="w-1/2 my-6 m-auto">
+            <button
+              className="bg-red text-center w-full m-auto py-3 rounded-xl text-white font-medium text-xl focus:outline-none"
+              value="Login"
+              onClick={loginUser}
+              type="submit"
+            >
+              Sign In
+            </button>
+          </div>
 
-                            )) }
-                            
-                        </select>
-                </div>
-               
-                <div className="bg-primary px-6 pt-4">
-                <label className="text-left text-white">Enter User Id</label>
-                        <input
-                        name="email_id"
-                        type="email"
-                        value={email_id}
-                        onChange={(e) => setEmail(e.target.value)}
-                            
-                            className={
-                                "w-full p-2 border-black border-2 outline-none text-sm transition duration-150 ease-in-out mb-4 mt-2"
-                            }
-                        />
-                </div>
-               
-                <div className="bg-primary px-6 pb-4">
-                <label className="text-white text-left">Password</label>
-                        <input
-                            name="password"
-                            type="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                           
-                            className={
-                                "w-full p-2 border-black border-2 outline-none text-sm transition duration-150 ease-in-out mb-4 mt-2"
-                            }
-                        />
-                </div>
-                </div>
-                <div className="bg-white w-full my-6">
-                    <button className="bg-green w-full py-4 text-white font-semibold text-2xl focus:outline-none"
-                      value="Login" onClick={loginUser} type="submit">Sign In</button>
-                </div>
-                <p className="text-center">New User ? <a href="/" className="underline">Sign Up</a></p>
-            </form>
-        </div>
-        {isOpen && <Popup
-      content={<>
-       
-        <p className="pb-4 text-red font-bold">{msg}</p>
-        <button className="bg-green px-10 py-2" onClick={loginUser}>Try Again</button>
-      </>}
-      handleClose={loginUser}
-    />}
-    </div>                     
-    )
+          <p className="text-center">
+            New User ?{" "}
+            <a href="/" className="underline">
+              Sign Up
+            </a>
+          </p>
+        </form>
+      </div>
+      {isOpen && (
+        <Popup
+          content={
+            <>
+              <p className="pb-4 text-red font-bold">{msg}</p>
+              <button className="bg-green px-10 py-2" onClick={loginUser}>
+                Try Again
+              </button>
+            </>
+          }
+          handleClose={loginUser}
+        />
+      )}
+    </div>
+  );
+};
 
-}
-
-export default Login
+export default Login;
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,155 +1,210 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import signup from '../popup';
-import options from '../levels';
-import Logo from '../images/logo.jpeg'
-import Popup from './Popup';
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import signup from "../popup";
+import options from "../levels";
+import Logo from "../images/logo.jpeg";
+import Popup from "./Popup";
+import chefLogin from "../images/chefLogin.png";
 
 const SignUp = () => {
-    const history = useHistory();
-    const [user, setUser] = useState({ fullName: "", email_id: "", contact: "", position: "", password: "" });
-    const [isOpen, setIsOpen] = useState(false);
-    const [attendence, setAttendence] = useState({status: "Session Not Started", checkinTime: "N/A",checkoutTime: "N/A", date: "N/A" })
-    const [msg, setMsg] = useState("");
-    const [isError, setIsError] = useState(false);
-    let name, value;
-    console.log(attendence)
-    const handleInputs = (e) => {
-        name = e.target.name;
-        value = e.target.value;
-        setUser({ ...user, [name]: value });
-    }
-    const onLogin = (e) => {
-        history.push('/login');
-    }
+  const history = useHistory();
+  const [user, setUser] = useState({
+    fullName: "",
+    email_id: "",
+    contact: "",
+    position: "",
+    password: "",
+  });
+  const [isOpen, setIsOpen] = useState(false);
+  const [attendence, setAttendence] = useState({
+    status: "Session Not Started",
+    checkinTime: "N/A",
+    checkoutTime: "N/A",
+    date: "N/A",
+  });
+  const [msg, setMsg] = useState("");
+  const [isError, setIsError] = useState(false);
+  let name, value;
+  console.log(attendence);
+  const handleInputs = (e) => {
+    name = e.target.name;
+    value = e.target.value;
+    setUser({ ...user, [name]: value });
+  };
+  const onLogin = (e) => {
+    history.push("/login");
+  };
 
-    const registerUser = async (e) => {
-        e.preventDefault();
-        const { fullName, email_id, contact, position, password } = user;
-        const res = await fetch("/app/signup", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                fullName, email_id, contact, position, password, attendence
-            })
-        });
-       
-        if (res.status === 201) {
-            let ans = "Registration Successful";
-            setMsg(ans);
-            setIsOpen(!isOpen);
-           
-        }
-        else {
-                let obj = signup.find((pop) => pop.id === res.status);
-                setMsg(obj.title);
-                setIsError(!isError);
-        }
+  const registerUser = async (e) => {
+    e.preventDefault();
+    const { fullName, email_id, contact, position, password } = user;
+    const res = await fetch("/app/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        fullName,
+        email_id,
+        contact,
+        position,
+        password,
+        attendence,
+      }),
+    });
+
+    if (res.status === 201) {
+      let ans = "Registration Successful";
+      setMsg(ans);
+      setIsOpen(!isOpen);
+    } else {
+      let obj = signup.find((pop) => pop.id === res.status);
+      setMsg(obj.title);
+      setIsError(!isError);
     }
+  };
 
+  return (
+    <div>
+      <div className="bg-white py-2">
+        <img className="mx-auto w-28 h-auto mb-4" src={Logo} alt="" />
+      </div>
+      <div className="mb-10">
+        <div className="flex flex-row h-auto m-auto bg-white w-3/4 rounded-3xl shadow-2xl">
+          <img src={chefLogin} />
 
-    return (
-        <div className="h-screen">
-            <div className="bg-gray-300 py-2">
-                <img className="mx-auto w-36 h-auto" src={Logo} alt="" />
-            </div>
+          <form className="w-1/2 md:w-1/3 mx-auto py-4 font-bold font-roboto text-lg justify">
             <div>
-                <form className="w-1/2 md:w-1/3 mx-auto py-4 font-bold font-roboto text-lg justify">
-                    <div className="shadow-2xl">
-                        <div className="bg-white px-6 py-4">
-                            <label className="text-left text-black mr-4">Position</label>
-                            <select name="position" className="p-2 border-black border-2 w-4/5 text-md" onChange={handleInputs} value={user.position}>
-                                <option className="bg-gray-300 border-black border-2 p-4">Select</option>
+              <div className="bg-white px-6 py-4">
+                <div className="w-full justify-center">
+                  <h1 className="text-red text-center text-2xl">Login</h1>
+                </div>
+                <label className="text-left text-black mr-4 font-normal">
+                  Position
+                </label>
+                <select
+                  name="position"
+                  className="p-2 border-2 w-full text-md bg-red rounded-lg  text-white  font-thin"
+                  onChange={handleInputs}
+                  value={user.position}
+                >
+                  <option value="none" selected="selected" hidden>
+                    Select Operator Type
+                  </option>
 
-                                {options.map((option) => (
-                                    <option value={option.value} className="bg-gray-300 border-black border-2 p-2">{option.label}</option>
+                  {options.map((option) => (
+                    <option
+                      value={option.value}
+                      className="bg-gray-300 border-black border-2 p-2"
+                    >
+                      {option.label}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className=" px-6 pt-4">
+                <label className="text-left font-normal">Fullname</label>
+                <input
+                  name="fullName"
+                  type="text"
+                  value={user.fullName}
+                  onChange={handleInputs}
+                  placeholder="Full Name"
+                  className={
+                    "w-full p-2 rounded-lg border-gray-200 border-2 outline-none text-sm transition duration-150 ease-in-out mb-4 mt-2"
+                  }
+                />
+              </div>
+              <div className=" px-6">
+                <label className="text-left font-normal  ">Email id</label>
+                <input
+                  name="email_id"
+                  type="email"
+                  value={user.email_id}
+                  onChange={handleInputs}
+                  placeholder="Email Id"
+                  className={
+                    "w-full p-2 rounded-lg border-gray-200 border-2 outline-none text-sm transition duration-150 ease-in-out mb-4 mt-2"
+                  }
+                />
+              </div>
+              <div className=" px-6">
+                <label className="text-left font-normal  ">
+                  Contact Number
+                </label>
+                <input
+                  name="contact"
+                  type="phone"
+                  value={user.contact}
+                  onChange={handleInputs}
+                  placeholder="Phone Number"
+                  className={
+                    "w-full p-2 rounded-lg border-gray-200 border-2 outline-none text-sm transition duration-150 ease-in-out mb-4 mt-2"
+                  }
+                />
+              </div>
 
-                                ))}
-
-                            </select>
-                        </div>
-                        <div className="bg-primary px-6 pt-4">
-                            <label className="text-left text-white">Fullname</label>
-                            <input
-                                name="fullName"
-                                type="text"
-                                value={user.fullName} onChange={handleInputs}
-
-                                className={
-                                    "w-full p-2 border-black border-2 outline-none text-sm transition duration-150 ease-in-out mb-4 mt-2"
-                                }
-                            />
-                        </div>
-                        <div className="bg-primary px-6">
-                            <label className="text-left text-white">Email id</label>
-                            <input
-                                name="email_id"
-                                type="email"
-                                value={user.email_id} onChange={handleInputs}
-
-                                className={
-                                    "w-full p-2 border-black border-2 outline-none text-sm transition duration-150 ease-in-out mb-4 mt-2"
-                                }
-                            />
-                        </div>
-                        <div className="bg-primary px-6">
-                            <label className="text-left text-white">Contact Number</label>
-                            <input
-                                name="contact"
-                                type="phone"
-                                value={user.contact} onChange={handleInputs}
-
-                                className={
-                                    "w-full p-2 border-black border-2 outline-none text-sm transition duration-150 ease-in-out mb-4 mt-2"
-                                }
-                            />
-                        </div>
-
-                        <div className="bg-primary px-6 pb-4">
-                            <label className="text-white text-left">Password</label>
-                            <input
-                                name="password"
-                                type="password"
-                                value={user.password}
-                                onChange={handleInputs}
-
-                                className={
-                                    "w-full p-2 border-black border-2 outline-none text-sm transition duration-150 ease-in-out mb-4 mt-2"
-                                }
-                            />
-                        </div>
-                    </div>
-                    <div className="bg-white w-full my-4 mb-2">
-                        <button className="bg-green w-full py-4 text-white font-semibold text-2xl focus:outline-none"
-                            value="Register" onClick={registerUser} type="submit">Register</button>
-                    </div>
-                    <p className="text-center">Already registered ? <a href="/login" className="underline">Login</a></p>
-                </form>
+              <div className=" px-6 pb-4">
+                <label className=" font-normal  text-left">Password</label>
+                <input
+                  name="password"
+                  type="password"
+                  value={user.password}
+                  onChange={handleInputs}
+                  placeholder="Password"
+                  className={
+                    "w-full p-2 rounded-lg border-gray-200 border-2 outline-none text-sm transition duration-150 ease-in-out mb-4 mt-2"
+                  }
+                />
+              </div>
             </div>
-            {isError && <Popup
-      content={<>
-       
-        <p className="pb-4 text-red font-bold">{msg}</p>
-        <button className="bg-green px-10 py-2" onClick={registerUser}>Try Again</button>
-      </>}
-      handleClose={registerUser}   
-    />}
-    {isOpen && <Popup
-                content={<>
+            <div className="w-1/2 my-3 m-auto">
+              <button
+                className="bg-red text-center w-full m-auto py-3 rounded-xl  text-white  font-medium text-xl focus:outline-none"
+                value="Register"
+                onClick={registerUser}
+                type="submit"
+              >
+                Register
+              </button>
+            </div>
+            <p className="text-center">
+              Already registered ?{" "}
+              <a href="/login" className="underline">
+                Login
+              </a>
+            </p>
+          </form>
+        </div>
+      </div>
+      {isError && (
+        <Popup
+          content={
+            <>
+              <p className="pb-4 text-red font-bold">{msg}</p>
+              <button className="bg-green px-10 py-2" onClick={registerUser}>
+                Try Again
+              </button>
+            </>
+          }
+          handleClose={registerUser}
+        />
+      )}
+      {isOpen && (
+        <Popup
+          content={
+            <>
+              <p className="pb-4 font-bold text-green">{msg}</p>
+              <button className=" px-10 py-2" onClick={onLogin}>
+                Ok
+              </button>
+            </>
+          }
+          handleClose={onLogin}
+        />
+      )}
+    </div>
+  );
+};
 
-                    <p className='pb-4 font-bold text-green'>{msg}</p>
-                    <button className="bg-primary px-10 py-2" onClick={onLogin}>Ok</button>
-                </>}
-                handleClose={onLogin}
-            />}
-  </div>
-       
-       
-
-
-    )
-}
-
-export default SignUp
+export default SignUp;
